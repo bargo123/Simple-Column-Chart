994d2bd993e9c9d515dcbb5bc83a19360693a2ee
zaid <zaid.barghouthi@equiti.com>
2022-03-12 13:12:20 +0300

    .

--- chart_app/lib/main.dart
+++ chart_app/lib/main.dart
@@ -9,49 +9,24 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: SimpleColumnChart(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class SimpleColumnChart extends StatefulWidget {
+  const SimpleColumnChart();
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SimpleColumnChart> createState() => SimpleColumnChartState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class SimpleColumnChartState extends State<SimpleColumnChart> {
   List<num> data = [50, 90, 80, 70, 60, 50, 40, 30, 20, 10];
-  StreamController<List<num>> _barsController = StreamController<List<num>>.broadcast();
+  final StreamController<List<num>> _barsController = StreamController<List<num>>.broadcast();
 
   @override
   void initState() {
@@ -146,24 +121,18 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class AnimatedBars extends StatefulWidget {
-  AnimatedBars({Key? key, this.color = Colors.transparent, this.value = 5}) : super(key: key);
+class AnimatedBars extends StatelessWidget {
+  AnimatedBars({Key? key, this.color = Colors.transparent, this.value = 5});
   final Color color;
   final num value;
-
-  @override
-  State<AnimatedBars> createState() => _AnimatedBarsState();
-}
-
-class _AnimatedBarsState extends State<AnimatedBars> {
   @override
   Widget build(BuildContext context) {
     return AnimatedSize(
         duration: const Duration(milliseconds: 599),
         child: Container(
           width: 30,
-          height: double.parse(widget.value.toString()) * 2.8,
-          color: widget.color,
+          height: double.parse(value.toString()) * 2.8,
+          color: color,
         ));
   }
 }
